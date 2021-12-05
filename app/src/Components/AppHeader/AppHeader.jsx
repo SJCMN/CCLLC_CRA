2ad7113c086be00353bf7c959d9519e5f8ab43ce
SJCMN <sam.clavette@gmail.com>
2021-12-05 14:42:53 -0600

    adjust icons on toolbar

--- app/src/Components/AppHeader/AppHeader.jsx
+++ app/src/Components/AppHeader/AppHeader.jsx
@@ -1,19 +1,80 @@
 import AppBar from '@mui/material/AppBar'
-// import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import MenuIcon from '@mui/icons-material/Menu';
+import IconButton from '@mui/material/IconButton';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import TwitterIcon from '@mui/icons-material/Twitter';
+
 
 function AppHeader() {
     return (
-        <AppBar position="relative">
-            <Toolbar>
-                {/* <CameraIcon sx={{ mr: 2 }} /> */}
-                <Typography variant="h6" color="inherit" noWrap>
-                    CCLLC_DEV
-                </Typography>
-            </Toolbar>
-        </AppBar>
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="relative">
+                <Toolbar>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="open drawer"
+                        sx={{ mr: 2 }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+
+                    <Typography 
+                        variant="h6" 
+                        color="inherit" 
+                        noWrap
+                        component="div"
+                        sx={{ display: { xs: 'none', sm: 'block' } }}
+                        >
+                        CCLLC_DEV
+                    </Typography>
+                    <Box sx={{ flexGrow: 1 }} />
+                    <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+                        <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        sx={{ mr: 2 }}>
+                            <GitHubIcon />
+                        </IconButton>
+
+                        <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        sx={{ mr: 2 }}>
+                            <LinkedInIcon />
+                        </IconButton>
+
+                        <IconButton
+                        size="large"
+                        edge="end"
+                        color="inherit"
+                        sx={{ mr: 2 }}>
+                            <TwitterIcon />
+                        </IconButton>
+                    </Box>  
+                </Toolbar>
+               
+            </AppBar>
+        </Box>
     )
 }
 
 export default AppHeader
+
+   
+    
+
+ 
+   
+    
+  
+
+
+
