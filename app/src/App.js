e2187c9f6d78f29d489f014aa07816c9a64b730c
SJCMN <sam.clavette@gmail.com>
2021-12-05 14:56:52 -0600

    add css

--- app/src/App.js
+++ app/src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import { HashRouter as Router } from 'react-router-dom';
 import StickyFooter from './Components/StickyFooter/StickyFooter';
 import AppHeader from './Components/AppHeader/AppHeader';
+import CssBaseline from '@mui/material/CssBaseline';
 
 
 function App() {
   return (
     <Router>
+      <CssBaseline />
       <div className="App">
         <AppHeader />
         <StickyFooter />
