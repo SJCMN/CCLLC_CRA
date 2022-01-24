ab14105b782638453c479dda2ab58d0d46b80b32
SJCMN <sam.clavette@gmail.com>
2022-01-24 15:16:44 -0600

    prettier formatter working

--- src/Components/DocumentViewer/DocumentViewer.jsx
+++ src/Components/DocumentViewer/DocumentViewer.jsx
@@ -1,68 +1,49 @@
-import React from 'react'
-import { useState } from 'react';
-import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
-import { Box, Paper } from '@mui/material'
+import React from "react";
+import { useState } from "react";
+import { Document, Page } from "react-pdf/dist/esm/entry.webpack";
+import { Box, Paper } from "@mui/material";
 
+function DocumentViewer() {
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
 
+  const onDocumentLoadSuccess = ({ numPages }) => {
+    setNumPages(numPages);
+    setPageNumber(1);
+  };
 
-function DocumentViewer () {
+  const changePage = (offset) => {
+    setPageNumber((prevPageNumber) => prevPageNumber + offset);
+  };
 
-    const [ numPages, setNumPages ] = useState(null);
-    const [ pageNumber, setPageNumber ] = useState(1);
+  const previousPage = () => {
+    changePage(-1);
+  };
 
-    const onDocumentLoadSuccess = ({ numPages }) => {
-        setNumPages(numPages);
-        setPageNumber(1);
-    };
+  const nextPage = () => {
+    changePage(1);
+  };
 
-    const changePage = (offset) => {
-        setPageNumber(prevPageNumber => prevPageNumber + offset);
-    };
-
-    const previousPage = () => {
-        changePage(-1);
-    };
-
-    const nextPage = () => {
-        changePage(1)
-    };
-
-  
-   
-
-    return (
-        <Paper 
-        elevation={2}
-        >
-            <Document
-            file = "./resume.pdf"
-            onLoadSuccess={onDocumentLoadSuccess}
-            >
-                <Page pageNumber={pageNumber} />
-            
-            </Document>
-                <p>
-                {pageNumber || (numPages ? 1 : '--')} of {numPages || '--'}
-                </p>
-                <button
-                    type="button"
-                    disabled={pageNumber <= 1}
-                    onClick={previousPage}
-                >
-                    ‹
-                </button>
-                <button
-                    type="button"
-                    disabled={pageNumber >= numPages}
-                    onClick={nextPage} 
-                >
-                    ›
-                </button>
-                
-                
-          
-        </Paper>
-    )
+  return (
+    <Paper elevation={2}>
+      <Document file="./resume.pdf" onLoadSuccess={onDocumentLoadSuccess}>
+        <Page pageNumber={pageNumber} />
+      </Document>
+      <p>
+        {pageNumber || (numPages ? 1 : "--")} of {numPages || "--"}
+      </p>
+      <button type="button" disabled={pageNumber <= 1} onClick={previousPage}>
+        ‹
+      </button>
+      <button
+        type="button"
+        disabled={pageNumber >= numPages}
+        onClick={nextPage}
+      >
+        ›
+      </button>
+    </Paper>
+  );
 }
 
 export default DocumentViewer;
--- src/Components/MediaCard/MediaCard.jsx
+++ src/Components/MediaCard/MediaCard.jsx
@@ -1,62 +1,43 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { CardActionArea, Link } from '@mui/material';
-import { useState } from 'react';
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea, Link } from "@mui/material";
+import { useState } from "react";
 
 export default function MediaCard({ galleryItem }) {
-  
-  const [gif, setGif] = useState(false)
+  const [gif, setGif] = useState(false);
   const showGif = () => {
-    setGif(!gif)
-  }
-
+    setGif(!gif);
+  };
 
   return (
     <Card sx={{ maxWidth: 345 }}>
-
       <CardActionArea>
-          <CardMedia
-            component="img"
-            height="440"
-            image={
-              gif === false ?
-                galleryItem.path : galleryItem.gif}
-            alt={galleryItem.title}
-            onMouseEnter={showGif}
-            onMouseLeave={showGif}
-            onTouchStart={showGif}
-            onTouchEnd={showGif}
-          />
+        <CardMedia
+          component="img"
+          height="440"
+          image={gif === false ? galleryItem.path : galleryItem.gif}
+          alt={galleryItem.title}
+          onMouseEnter={showGif}
+          onMouseLeave={showGif}
+          onTouchStart={showGif}
+          onTouchEnd={showGif}
+        />
+      </CardActionArea>
 
-        </CardActionArea>
-
-        <CardContent sx={{ flexGrow: 1 }}>
-          
-          <Typography 
-          gutterBottom 
-          variant="h5" 
-          component="div"
-          >  
-          <Link
-          href={galleryItem.link}
-          underline="none"
-          color="inherit"
-          >
-          {galleryItem.title}
+      <CardContent sx={{ flexGrow: 1 }}>
+        <Typography gutterBottom variant="h5" component="div">
+          <Link href={galleryItem.link} underline="none" color="inherit">
+            {galleryItem.title}
           </Link>
-          </Typography>
-          
-          <Typography variant="body2" color="text.secondary">
-            {galleryItem.description}
-          </Typography>
-          
-        </CardContent>
-        
-      
+        </Typography>
 
+        <Typography variant="body2" color="text.secondary">
+          {galleryItem.description}
+        </Typography>
+      </CardContent>
     </Card>
   );
 }
